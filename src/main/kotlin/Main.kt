c093bdf71e4b77eb17dafd84a696a1939d29bbf3
Vasilii Karmanov <oldheffalump1@gmail.com>
2023-05-18 10:53:32 +0300

    Add a comment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,10 +4,13 @@ fun main(args: Array<String>) {
     val minimumDiscount: Int = 100
     val discountPercentFull: Double = 0.01
     val discountPercent: Double = 0.05
+    val noDiscountValue: Int = 1_000
+    val firstDiscountLevel: Int = 1_001
+    val noDiscountResult: Int = 0
 
-    val discount = if (purchase <= 1_000) {
-        0
-    } else if (purchase > 1_000 && purchase < 10_001) {
+    val discount = if (purchase <= noDiscountValue) {
+        noDiscountResult
+    } else if (purchase > noDiscountValue && purchase < firstDiscountLevel) {
         if (regularClient) {
             (purchase * discountPercentFull) + minimumDiscount // скидка для постоянного клиента
         } else {
@@ -17,10 +20,15 @@ fun main(args: Array<String>) {
     else {
         if (regularClient) {
             (purchase * discountPercent) + ((purchase - (purchase * discountPercent)) * discountPercentFull)
-        // Скидка для постоянного клиента от 10_001р через две операции. Уверен можно проще.
-        } else {
-            (purchase * discountPercent)
-        }
-    }
-    println("Размер скидки $discount рублей")
+/*
+Скидка для постоянного клиента от 10_001р через две операции.
+Сначала я вычисли значение обычной скидки((purchase * discountPercent))
+Затем вычел это значение из суммы следующей скидки (((purchase - (purchase * discountPercent)) * discountPercentFull))
+Возможно я неправильно понял условие.
+*/
+} else {
+    (purchase * discountPercent) // Скидка не для постоянного клиента
+}
+}
+println("Размер скидки $discount рублей")
 }
